98621bf1abecb6e86cb2e6e828326e606b3d0bfa
Lovepreet Kaur <747018@student.tor.loc>
2019-08-07 16:29:20 -0400

    fourth commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-//const http=require('http');
+/*const http=require('http');
 
-//const site=http.createServer(function(req,res){
-    //console.log('Hello World');
-    //res.setHeader('Content-Type','text/html');
-    // res.write('I am a Web Server');
-    //res.end('<h1> Hello Class!!</h1>');
-//});
+const site=http.createServer(function(req,res){
+    console.log('Hello World');
+    res.setHeader('Content-Type','text/html');
+     res.write('I am a Web Server');
+    res.end('<h1> Hello Class!!</h1>');
+});
 
-//site.listen(3000);
+site.listen(3000);*/
+
 
 var express=require('express')
 var app=express()
